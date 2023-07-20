b5b99fee0cf51688220f700b94fc7456c5fae32f
Agustin Garrido <garriagus@gmail.com>
2023-07-20 17:34:34 -0300

    fix db

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-
+from import_export.admin import ImportExportMixin
 from .models import Docente, Alumno, Asignatura, Curso
-admin.site.register(Docente)
-admin.site.register(Alumno)
-admin.site.register(Asignatura)
+
+class DocenteAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'fecha_nacimiento', 'documento', 'ingreso_cargo']
+
+class AlumnoAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['nombre', 'apellido', 'fecha_nacimiento', 'documento', 'ingreso_institucion', 'curso_id']
+
+class AsignaturaAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ['asignatura', 'curso_id', 'docente_id']
+
+
+admin.site.register(Docente, DocenteAdmin)
+admin.site.register(Alumno, AlumnoAdmin)
+admin.site.register(Asignatura, AsignaturaAdmin)
 admin.site.register(Curso)
